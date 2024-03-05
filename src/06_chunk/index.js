79bd77cd0236b3b6d568c61dc6bde749fdfa701c
Dmytro Novikov <nodm.pe@gmail.com>
2024-03-05 17:24:34 +0200

    S06: Splitting an array into chunks

---
+++ src/06_chunk/index.js
@@ -0,0 +1,23 @@
+// --- Directions
+// Given an array and chunk size, divide the array into many subarrays
+// where each subarray is of length size
+// --- Examples
+// chunk([1, 2, 3, 4], 2) --> [[ 1, 2], [3, 4]]
+// chunk([1, 2, 3, 4, 5], 2) --> [[ 1, 2], [3, 4], [5]]
+// chunk([1, 2, 3, 4, 5, 6, 7, 8], 3) --> [[ 1, 2, 3], [4, 5, 6], [7, 8]]
+// chunk([1, 2, 3, 4, 5], 4) --> [[ 1, 2, 3, 4], [5]]
+// chunk([1, 2, 3, 4, 5], 10) --> [[ 1, 2, 3, 4, 5]]
+
+function chunk(array, size) {
+  const chunks = [];
+  let startIndex = 0;
+
+  while (startIndex < array.length) {
+    chunks.push(array.slice(startIndex, startIndex + size));
+    startIndex += size;
+  }
+
+  return chunks;
+}
+
+module.exports = chunk;
