7729fc7f6907537da612b850c8bc9f6891908d27
Dmytro Novikov <nodm.pe@gmail.com>
2024-03-06 22:07:31 +0200

    S11: Count vowels

---
+++ src/11_vowels/index.js
@@ -0,0 +1,16 @@
+// --- Directions
+// Write a function that returns the number of vowels
+// used in a string.  Vowels are the characters 'a', 'e'
+// 'i', 'o', and 'u'.
+// --- Examples
+//   vowels('Hi There!') --> 3
+//   vowels('Why do you ask?') --> 4
+//   vowels('Why?') --> 0
+
+const vowelsRegex = /[aeiou]/ig;
+
+function vowels(str) {
+  return str.match(vowelsRegex)?.length ?? 0;
+}
+
+module.exports = vowels;
